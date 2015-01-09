63755c01d56a9411708ef5da40bb05833b6e7ca9
Sunny-J <lutics@users.noreply.github.com>
2015-01-09 09:25:30 +0900

    Providing Up Navigation 예제 추가

--- mobile/src/main/java/io/lutics/app/android/activity/IntroActivity.java
+++ mobile/src/main/java/io/lutics/app/android/activity/IntroActivity.java
@@ -1,16 +1,20 @@
 package io.lutics.app.android.activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import io.lutics.app.android.R;
 import io.lutics.app.android.util.GcmRegistrationAsyncTask;
 
-public class IntroActivity extends BaseActivity {
+public class IntroActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
         new GcmRegistrationAsyncTask(this).execute();
+
+        startActivity(new Intent(this, SettingsActivity.class));
     }
 }
